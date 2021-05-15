579c5a4149b88bd22393bec977d996b92063265c
AndreyTungulin21 <Tungulin@icloud.com>
2021-05-15 14:29:34 +0300

    Добавлена смерть

--- Assets/Scripts/SriptMenu/MainMenu.cs
+++ Assets/Scripts/SriptMenu/MainMenu.cs
@@ -7,7 +7,8 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        Time.timeScale = 1;
+        SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex) ;
     }
 
     public void Quit()
